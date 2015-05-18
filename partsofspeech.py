24a140e4cd2082acfa2689dbbe5684ce4c3d348d
amalene <amalene@mit.edu>
2015-05-18 18:36:16 -0400

    project complete

--- partsofspeech.py
+++ partsofspeech.py
@@ -3,6 +3,7 @@ import plotly.plotly as py
 import cPickle as pickle
 from plotly.graph_objs import *
 from textblob import TextBlob
+import operator
 
 playkeys = ['AllsWellThatEndsWell', 'AntonyAndCleopatra', 'AsYouLikeIt',
             'ComedyOfErrors', 'Coriolanus', 'Cymbeline', 'Hamlet', 'HenryIV',
@@ -37,41 +38,114 @@ tragedies = ['AntonyAndCleopatra', 'Coriolanus', 'Hamlet', 'JuliusCaesar', 'King
 histories = ['HenryIV', 'HenryV', 'HenryVIII', 'HenryVIPart1', 'HenryVIPart2', 'HenryVIPart3',
            'King John', 'Richard II', 'Richard III']
 
-fm = open('malepos.pkl', 'w')
-ff = open('femalepos.pkl', 'w')
+fm = pickle.load(open('malepos.pkl', 'r+'))
+ff = pickle.load(open('femalepos.pkl', 'r+'))
 
-allmpos = {}
-allfpos = {}
+##allmpos = {}
+##allfpos = {}
+##
+##mpos = {}
+##fpos = {}
+##
+##count = 0
+##
+##for i in range(0,len(playkeys)):
+##    f = open(str(filenames[i]) + '.pkl', 'r+')
+##    play = pickle.load(f)
+##    playname = playkeys[i]
+##    data = play[playname]
+##    print playname
+##    for j in data:
+##        blob = TextBlob(j['text_entry'])
+##        for tag in blob.tags:
+##            if count < 10:
+##                print tag[1]
+##                count += 1
+##            if j['gender'] == 'm':
+##                if tag[1] == '\n':
+##                    print tag
+##                    print tag[1]
+##                if tag[1] in mpos:
+##                    mpos[tag[1]] += 1
+##                else:
+##                    mpos[tag[1]] = 1
+##            elif j['gender'] == 'f':
+##                if tag[1] in fpos:
+##                    fpos[tag[1]] += 1
+##                else:
+##                    fpos[tag[1]] = 1
+##    allmpos[playname] = mpos
+##    allfpos[playname] = fpos
+##    mpos = {}
+##    fpos = {}
+##
+##fm.seek(0)
+##ff.seek(0)
+##        
+##pickle.dump(allmpos, fm)
+##pickle.dump(allfpos, ff)            
 
-mpos = {}
-fpos = {}
+totalm = {}
+totalf = {}
+comedym = {}
+comedyf = {}
+tragedym = {}
+tragedyf = {}
+historym = {}
+historyf = {}
+wordsm = 0
+wordsf = 0
 
-for i in range(0,len(playkeys)):
-    f = open(str(filenames[i]) + '.pkl', 'r+')
-    play = pickle.load(f)
-    playname = playkeys[i]
-    data = play[playname]
-    print playname
-    for j in data:
-        blob = Textblob(j['text_entry'])
-        for tag in blob.tags:
-            if j['gender'] == 'm':
-                if tag[1] in mpos:
-                    mpos[tag[1]] += 1
-                else:
-                    mpos[tag[1]] = 1
-            elif j['gender'] == 'f':
-                if tag[1] in fpos:
-                    mpos[tag[1]] += 1
-                else:
-                    fpos[tag[1]] = 1
-    allmpos[playname] = mpos
-    allfpos[playname] = fpos
-    mpos = {}
-    fpos = {}
-
-
-        
-pickle.dump(allmpos, open('malepos.pkl', 'r+'))
-pickle.dump(allfpos, open('femalepos.pkl', 'r+'))            
+count = 0
             
+for play in fm:
+    print play
+    for key in fm[play]:
+        wordsm += fm[play][key]
+        if count < 10:
+            count+= 1
+        if key in totalm:
+            totalm[key] += fm[play][key]
+        else:
+            totalm[key] = fm[play][key]
+        if play in comedies:
+            if key in comedym:
+                comedym[key] += fm[play][key]
+            else:
+                comedym[key] = fm[play][key]
+        elif play in tragedies:
+            if key in tragedym:
+                tragedym[key] += fm[play][key]
+            else:
+                tragedym[key] = fm[play][key]
+        elif play in histories:
+            if key in historym:
+                historym[key] += fm[play][key]
+            else:
+                historym[key] = fm[play][key]
+        
+for play in ff:
+    for key in ff[play]:
+        wordsf += ff[play][key]
+        if key in totalf:
+            totalf[key] += ff[play][key]
+        else:
+            totalf[key] = ff[play][key]
+        if play in comedies:
+            if key in comedyf:
+                comedyf[key] += ff[play][key]
+            else:
+                comedyf[key] = ff[play][key]
+        elif play in tragedies:
+            if key in tragedyf:
+                tragedyf[key] += ff[play][key]
+            else:
+                tragedyf[key] = ff[play][key]
+        elif play in histories:
+            if key in historyf:
+                historyf[key] += ff[play][key]
+            else:
+                historyf[key] = ff[play][key]
+            
+sorted_m = sorted(totalm.items(), key=operator.itemgetter(1))
+sorted_f = sorted(totalf.items(), key=operator.itemgetter(1))
--- polarity.py
+++ polarity.py
@@ -2,6 +2,22 @@ from __future__ import division
 from collections import namedtuple
 import cPickle as pickle
 from textblob import TextBlob
+import plotly.plotly as py
+from plotly.graph_objs import *
+
+comedies = ['AllsWellThatEndsWell', 'AsYouLikeIt','ComedyOfErrors', 'Cymbeline',
+            'LovesLaboursLost', 'Measure For Measure', 'MerchantOfVenice',
+            'MerryWivesOfWindsor', 'Midsummer Nights Dream', 'Much Ado About Nothing',
+            'Pericles', 'Taming Of The Shrew', 'The Tempest', 'Troilus and Cressida',
+            'Twelfth Night', 'Two Gentlemen of Verona', "The Winter's Tale"]
+
+tragedies = ['AntonyAndCleopatra', 'Coriolanus', 'Hamlet', 'JuliusCaesar', 'King Lear', 'Macbeth',
+            'Othello', 'Romeo and Juliet', 'Timon of Athens', 'Titus Andronicus']
+
+histories = ['HenryIV', 'HenryV', 'HenryVIII', 'HenryVIPart1', 'HenryVIPart2', 'HenryVIPart3',
+           'King John', 'Richard II', 'Richard III']
+
+
 
 f = open('TitusAndronicus.pkl', 'r+')
 play = pickle.load(f)
@@ -124,3 +140,70 @@ print "female polarity: " + str(averagefemalepol)
 print "male polarity: " + str(averagemalepol)
 print "netfemale polarity: " + str(averagenetfemalepol)
 print "netmale polarity: " + str(averagenetmalepol)
+
+y1 = []
+y2 = []
+x1 = comedies
+for i in x1:
+    y1.append(ma[i])
+    y2.append(fa[i])
+
+trace1 = Bar(x=x1, y=y1, name='men')
+trace2 = Bar(x=x1, y=y2, name='women')
+
+data = Data([trace1, trace2])
+layout=Layout(
+    barmode='group',
+    title="Standard Polarity in Comedies")
+fig=Figure(data=data, layout=layout)
+plot_url = py.plot(fig, filename='Standard Polarity - Comedies')
+
+
+
+
+comedysm = 0
+tragedysm = 0
+historysm = 0
+comedypm = 0
+tragedypm = 0
+historypm = 0
+
+comedysf = 0
+tragedysf = 0
+historysf = 0
+comedypf = 0
+tragedypf = 0
+historypf = 0
+
+for play in comedies:
+    comedysm += ms[play]
+    comedypm += mp[play]
+    comedysf += fs[play]
+    comedypf += fs[play]
+
+for play in tragedies:
+    tragedysm += ms[play]
+    tragedypm += mp[play]
+    tragedysf += fs[play]
+    tragedypf += fs[play]
+
+for play in histories:
+    historysm += ms[play]
+    historypm += mp[play]
+    historysf += fs[play]
+    historypf += fs[play]
+
+comedysm = comedysm/len(comedies)
+comedypm = comedypm/len(comedies)
+comedysf = comedysf/len(comedies)
+comedypf = comedypf/len(comedies)
+
+tragedysm = tragedysm/len(tragedies)
+tragedypm = tragedypm/len(tragedies)
+tragedysf = tragedysf/len(tragedies)
+tragedypf = tragedypf/len(tragedies)
+
+historysm = historysm/len(histories)
+historypm = historypm/len(histories)
+historysf = historysf/len(histories)
+historypf = historypf/len(histories)
